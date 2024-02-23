07f6a00220e44229e92fb5123ef48c56df326bda
SGuevaraOlie <santiguevara98@gmail.com>
2024-02-23 17:37:18 -0300

    about

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -8,11 +8,6 @@ const TAB_DATA = [
         id: 'Tecnologías',
         content:(
             <div className='flex flex-row justify-between text-xs lg:text-base'>
-                <div className='flex flex-col'>
-                    <h1 className='mb-2'>Idiomas</h1>
-                    <li>Javascript</li>
-                    <li>TypeScript</li>
-                </div>
                 <div className='flex-flex-col'>
                     <h1 className='mb-2'>Front-End</h1>
                     <li>HTML</li>
@@ -32,6 +27,11 @@ const TAB_DATA = [
                     <h1 className='mb-2'>Database</h1>
                     <li>Sequelize</li>
                     <li>PostgreSQL</li>
+                </div>
+                <div className='flex flex-col'>
+                    <h1 className='mb-2'>Idiomas</h1>
+                    <li>Javascript</li>
+                    <li>TypeScript</li>
                 </div>
             </div>
         )
@@ -75,19 +75,10 @@ const TAB_DATA = [
         title: 'Educación',
         id: 'Educación',
         content:(
-            <div className='flex flex-row justify-around mt-2 text-xs lg:text-base'>
-                <div>
-                    <h1>SoyHenry</h1>
-                    <p>Fullstack Web Developer</p>
-                </div>
-                <div>
-                    <h1>Argentina Programa</h1>
-                    <p>Introducción a la programación</p>
-                </div>
-                <div>
-                    <h1>Arrow Academy</h1>
-                    <p>Introducción a Phyton / POO / DJANGO</p>
-                </div>
+            <div className='flex flex-col mt-2 text-xs lg:text-base'>
+                    <li>SoyHenry Fullstack Web Developer</li>
+                    <li>Argentina Programa Introducción a la programación</li>
+                    <li>Arrow Academy Introducción a Phyton / POO / DJANGO</li>
             </div>
         )
     },
@@ -103,11 +94,11 @@ const AboutMe = () => {
     }
     return (
         <section className='text-white'>
-            <div className='flex flex-col lg:flex-row justify-center items-center mt-56 mb-32 lg:mt-72 lg:mb-48'>
+            <div className='flex flex-col lg:flex-row justify-center items-center mt-48 mb-24s lg:mt-72 lg:mb-48'>
                 <img src='\imagenes\skeleton-typing.gif' alt='esqueleto :3' className='rounded rounded-full h-[80%] w-[80%] lg:mr-48 lg:-ml-2'/>
                 <div className='flex flex-col h-[400px] mt-4 lg:mt-0'>
                     <h2 className='text-[#ffb826] font-bold mt-8 lg:mt-0 text-2xl lg:text-4xl'>Sobre mí</h2>
-                    <p className="text-[#ADB7BE] text-sm lg:text-lg mt-4">Soy un Desarrollador Web Full-Stack con orientación a Front-End, con una firme motivación por ampliar mis conocimientos en todas las áreas del desarrollo web. Soy una persona detallista, eficiente y productiva, que valora trabajar siguiendo buenas prácticas y tiene habilidades efectivas de comunicación, liderazgo y trabajo en equipo.</p>
+                    <p className="text-[#ADB7BE] text-xs lg:text-lg mt-4">Soy un Desarrollador Web Full-Stack con orientación a Front-End, con una firme motivación por ampliar mis conocimientos en todas las áreas del desarrollo web. Soy una persona detallista, eficiente y productiva, que valora trabajar siguiendo buenas prácticas y tiene habilidades efectivas de comunicación, liderazgo y trabajo en equipo.</p>
                     <div className='flex flex-row justify-between mt-8 text-xs lg:text-base'>
                         <TabButton selectTab={() => handleTabChange('Tecnologías')} active={tab === 'Tecnologías'}>
                             {" "}

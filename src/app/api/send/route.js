8c49779d97112de493ae896f54fb107d60d3e810
SGuevaraOlie <santiguevara98@gmail.com>
2024-03-01 01:19:11 -0300

    resend domain

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -2,16 +2,14 @@ import { NextResponse } from "next/server";
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail = process.env.FROM_EMAIL;
-const toEmail = process.env.TO_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
     const data = await resend.emails.send({
-      from: fromEmail,
-      to: [toEmail, email],
+      from: 'onboarding@resend.dev',
+      to: ['sguevaraolie.dev@gmail.com', email],
       subject: subject,
       react: (
         <>
